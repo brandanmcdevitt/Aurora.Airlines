69d49932577eb22a6329d2f49e70e1008626b191
brandanmcdevitt <brandan.mcdevitt@gmail.com>
2018-04-24 17:51:16 +0100

    Changing standard php tagged comments to laravels syntax

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,19 +1,14 @@
-<?php
-
-/*
-
+{{--
+    
 Creating the header.php to allow us to include it to every page with a simple function.
 
-*/
+--}}
 
-?>
     <!DOCTYPE html>
     <html>
-
     <head>
-        <?php
         
-        /*
+        {{--
         
         setting up variables to use throughout the website
         
@@ -22,29 +17,22 @@ Creating the header.php to allow us to include it to every page with a simple fu
         $pageTitle will be used on the single pages to display the current file name
         as the page title; minus the .php file extension on the end
         
-        */
+        --}} 
         
+        <?php
         $title = "Aurora Airlines";
         $pageTitle = basename($_SERVER['PHP_SELF'],'.php');
         ?>
 
-            <?php 
         
-        //linking .css and .js files
+        {{-- linking .css and .js files --}}
         
-        ?>
             <link href="{{ url('css/main.css') }}" rel="stylesheet" type="text/css" />
             <link href="{{ url('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
-
             <script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
-            <!-- <script src="/js/jquery.min.js"></script> -->
-            <!-- <script src="/js/owl.carousel.min.js"></script> -->
-
-            <?php
         
-        //printing the $title
+        {{-- printing the $title --}}
         
-        ?>
                 <title>
                     {{ $title }}
                 </title>
@@ -52,31 +40,24 @@ Creating the header.php to allow us to include it to every page with a simple fu
 
     <body>
 
-        <?php
-        
-        /*
-        
-        if the page title is equal to "index" then display a specific div.
-        if not, then display another div.
-        
-        */
-        
-        ?>
 
-            @if($pageTitle == "index")
-            <div class="home-nav">
-                @else
-                <div class="nav">
-                    @endif
 
-                    <div class="logo"><a href="/"><img src="images/logo.png"></a>
-                    </div>
+        {{-- if the page title is equal to "index" then display a specific div. if not, then display another div. --}} 
+        
+        @if($pageTitle == "index")
+        <div class="home-nav">
+            @else
+            <div class="nav">
+                @endif
 
-                    <div class="nav-links">
-                        <a href="/">Home</a>
-                        <a href="destinations">Destinations</a>
-                        <a href="contact">Contact Us</a>
-                        <a href="login">Log In</a>
-                    </div>
-
+                <div class="logo"><a href="/"><img src="images/logo.png"></a>
                 </div>
+
+                <div class="nav-links">
+                    <a href="/">Home</a>
+                    <a href="destinations">Destinations</a>
+                    <a href="contact">Contact Us</a>
+                    <a href="login">Log In</a>
+                </div>
+
+            </div>
