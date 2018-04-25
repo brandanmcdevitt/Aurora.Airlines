eb0442e212dd83330af64b9866592c0c8ff68336
brandanmcdevitt <brandan.mcdevitt@gmail.com>
2018-04-25 15:41:35 +0100

    Populated featured page

--- resources/views/pages/featured.blade.php
+++ resources/views/pages/featured.blade.php
@@ -9,6 +9,27 @@
 <div class="half-content fea-text">
     <h2>City-Break</h2>
 <p>(The doc picks up a remote control device.) Watch this. Not me, the car, the car. My calculations are correct, when this baby hits eighty-eight miles per hour, your gonna see some serious shit. Watch this, watch this. (The car heads straight for them picking up speed all the while. It hits 88 MPH just as it reaches them and it disappears. Leaving behind fire track marks on the pavement.) Ha, what did I tell you, eighty-eight miles per hour. The temporal displacement occurred at exactly 1:20 a.m. and zero seconds. Jesus Christ, Doc, you disintegrated Einstein. Calm down, Marty, I didn't disintegrate anything. The molecular structure of Einstein and the car are completely intact. Where the hell are they.</p>
+    
+    <p>Uh...it means that you win without a fight. Without shootin'? Hey, he can't do that. (to Marty) You can't do that! (Marty gulps.) You know what I think? Ah think you're a gutless yella turd. And I'm giving ya to the count of 10 to come out here and prove I'm wrong. 1... Doc!! C'mon, sober up, buddy, let's go. 2... You better get out there son - I got $20 goin' betting on ya so you can't let me down.</p>
 </div>
+
+<div class="fea-p-text">
+<p>Just turn around, McFly, and walk away. Are you deaf, McFly? Close the door and beat it. No, Biff, you leave her alone. Alright, McFly, you're asking for it, and now you're gonna get it. (Biff grabs George's arm and twists it.) Biff, stop it. Biff, you're breaking his arm. Biff, stop.Yeah? (v.o) Where're you going Biff? I'm going to get my car Grandma! (v.o) When are you coming back? My feet hurt and I want you to rub my toes some more. (to himself) Shut up you old bag.Buttheads. (Marty comes out of the underground entrance of Courthouse Mall and sees the police outside the Mall. A crowd has also gathered. A button flashes on Marty's jacket and he presses it.) (v.o) Drying mode on. Jacket drying. (beep) Your jacket is now dry. (Marty walks over to the girls he borrowed the hoverboard from.) Hey kid, little girl, thanks.Oh, well, that would be very gentlemanly of you, Mr. Brown...Emmett. You know, I'm almost glad that snake spooked those horses. Otherwise, we might never have met. I suppose it was destiny. Well, thank you for everything. You're quite welcome. I will...see you again, won't I? Of course, you'll see lots of me, I'm sure, I have a shop in town. I'm a local scientist...uh...uh...blacksmith. Science? What sort of science? Astronomy? Chemistry?</p>
+    
+    <p>There's no point in denying it. I'm in love with Clara. Oh man. Doc, we don't belong here! Neither one of us! You know it could still be you that gets shot tomorrow! (He takes out the photograph of the tombstone from his pocket and shoves it in Doc's face.) This tombstone could still be in your future! Marty...the future isn't written. It can be changed; you know that! Anyone can make their future whatever they want it to be. I can't let this one little photograph determine my entire destiny. I have to live my life according to what I believe is right... in my heart. (sighs) Doc...you're a scientist. So you tell me. What's the right thing to do, up here?? (he points to his head.) (thinks and then sighs) You're right, Marty.</p>
+    
+    <ul>
+    <li>(A few minutes later Doc and Marty are studying a map on the wall of the train station.)</li>
+    <li>And that crater in the middle northwest, out there all by itself like a starburst?</li>
+    <li>I have to tell you about the future. Huh? I have to tell you about the future. (Doc almost falls off the Clock Tower.) Ahh.</li>
+    <li>Well, then, step right up like a man.</li>
+    <li>(Later) (Marty is reading the letter aloud and Doc is looking at the circuit control microchip though a magnifying glass.)</li>
+    </ul>
+    
+    <p>I told you to watch your back, Smithy. (surprised) Tannen. But you're early. It's a Derringer, Smithy. Small but effective. Last time I used it the fella took two days to die. Bled to death inside, it was real painful. (the gang laughs) That means you'd be dead by about suppertime Monday. I don't know who you think you are, but we're dancing. Well looky what we have here...introduce me to the lady; I'd like a dance.</p>
+</div>
+
+<br/>
+<br/>
 
 @include('inc.footer')
